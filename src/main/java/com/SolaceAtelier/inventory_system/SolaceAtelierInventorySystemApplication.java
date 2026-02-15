78fc3b314ccdc728633a6b2af93ab176fe31d421
Trish Figueroa <jxt_04@outlook.com>
2026-02-15 17:31:28 -0500

    Initial Spring Boot project (Web + Thymeleaf)

---
+++ src/main/java/com/SolaceAtelier/inventory_system/SolaceAtelierInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.SolaceAtelier.inventory_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolaceAtelierInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolaceAtelierInventorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SolaceAtelier/inventory_system/SolaceAtelierInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SolaceAtelier.inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolaceAtelierInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
